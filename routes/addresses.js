df2e1efdd1dc6ee53fc557485a074fb2290a21f6
Jason Creviston <jcreviston@weendeavor.com>
2019-05-29 15:54:55 -0400

    added addresses.js

---
+++ routes/addresses.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
+// load prefix model
+require('../models/Prefix');
+const Prefix = mongoose.model('prefixes');
+
+// load site model
+require('../models/Site');
+const Site = mongoose.model('sites');
+
+// load address model
+require('../models/Address');
+const Address = mongoose.model('addresses');
+
+// add prefix route
+router.get('/add', (req, res) => {
+  res.render('addresses/add');
+});
+
+
+
+module.exports = router;
